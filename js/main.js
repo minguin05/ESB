5bdf3ff887c4febfddd2276ef6bb6fa81c95d19e
minguin05 <sjrndnfl@naver.com>
2021-03-31 22:36:12 +0900

    초기 설정

---
+++ js/main.js
@@ -0,0 +1,179 @@
+$(function() {
+  "use strict";
+
+  //------- Parallax -------//
+  skrollr.init({
+    forceHeight: false
+  });
+
+  //------- Active Nice Select --------//
+  $('select').niceSelect();
+
+  //------- hero carousel -------//
+  $(".hero-carousel").owlCarousel({
+    items:3,
+    margin: 10,
+    autoplay:false,
+    autoplayTimeout: 5000,
+    loop:true,
+    nav:false,
+    dots:false,
+    responsive:{
+      0:{
+        items:1
+      },
+      600:{
+        items: 2
+      },
+      810:{
+        items:3
+      }
+    }
+  });
+
+  var window_w = $(window).innerWidth();
+
+  //------- Best Seller Carousel -------//
+  if($('.owl-carousel').length > 0){
+    $('#bestSellerCarousel').owlCarousel({
+      loop:true,
+      margin:30,
+      nav:true,
+      navText: ["<i class='ti-arrow-left'></i>","<i class='ti-arrow-right'></i>"],
+      dots: false,
+      responsive:{
+        0:{
+          items:1
+        },
+        600:{
+          items: 2
+        },
+        900:{
+          items:3
+        },
+        1130:{
+          items:4
+        }
+      }
+    })
+  }
+
+  /*------------------
+		Background set
+	--------------------*/
+	$('.set-bg').each(function() {
+		var bg = $(this).data('setbg');
+		$(this).css('background-image', 'url(' + bg + ')');
+	});
+
+	
+	/*------------------
+		Hero Slider
+	--------------------*/
+	var window_h = $(window).innerHeight();
+	var header_h = $('.header-section').innerHeight();
+	var nav_h = $('.nav-section').innerHeight();
+
+	if (window_w > 767) {
+		$('.hs-item').height((window_h)-((header_h)+(nav_h)));
+	}
+
+	$('.hero-slider').owlCarousel({
+		loop: true,
+		nav: false,
+		dots: true,
+		mouseDrag: false,
+		animateOut: 'fadeOut',
+    	animateIn: 'fadeIn',
+		items: 1,
+		autoplay: true
+	});
+
+  /*------------------
+		Gallery
+	--------------------*/
+	$('.gallery').find('.gallery-item').each(function() {
+		var pi_height1 = $(this).width(),
+		pi_height2 = pi_height1/2;
+		
+		if($(this).hasClass('gi-long') && window_w > 991){
+			$(this).css('height', pi_height2);
+		}else{
+			$(this).css('height', Math.abs(pi_height1));
+		}
+	});
+
+	$('.gallery').masonry({
+		itemSelector: '.gallery-item',
+		columnWidth: '.grid-sizer'
+	});
+
+  //------- single product area carousel -------//
+  $(".s_Product_carousel").owlCarousel({
+    items:1,
+    autoplay:false,
+    autoplayTimeout: 5000,
+    loop:true,
+    nav:false,
+    dots:false
+  });
+
+  /*------------------
+		Popup
+	--------------------*/
+	$('.img-popup').magnificPopup({
+		type: 'image',
+		mainClass: 'img-popup-warp',
+		removalDelay: 400,
+	});
+
+  //------- mailchimp --------//  
+	function mailChimp() {
+		$('#mc_embed_signup').find('form').ajaxChimp();
+	}
+  mailChimp();
+  
+  //------- fixed navbar --------//  
+  $(window).scroll(function(){
+    var sticky = $('.header_area'),
+    scroll = $(window).scrollTop();
+
+    if (scroll >= 100) sticky.addClass('fixed');
+    else sticky.removeClass('fixed');
+  });
+
+  //------- Price Range slider -------//
+  if(document.getElementById("price-range")){
+  
+    var nonLinearSlider = document.getElementById('price-range');
+    
+    noUiSlider.create(nonLinearSlider, {
+        connect: true,
+        behaviour: 'tap',
+        start: [ 500, 4000 ],
+        range: {
+            // Starting at 500, step the value by 500,
+            // until 4000 is reached. From there, step by 1000.
+            'min': [ 0 ],
+            '10%': [ 500, 500 ],
+            '50%': [ 4000, 1000 ],
+            'max': [ 10000 ]
+        }
+    });
+    
+  
+    var nodes = [
+        document.getElementById('lower-value'), // 0
+        document.getElementById('upper-value')  // 1
+    ];
+  
+    // Display the slider value and how far the handle moved
+    // from the left edge of the slider.
+    nonLinearSlider.noUiSlider.on('update', function ( values, handle, unencoded, isTap, positions ) {
+        nodes[handle].innerHTML = values[handle];
+    });
+  
+  }
+  
+});
+
